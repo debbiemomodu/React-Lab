7e60717840578a97e3bd2ae77ceb8dca3f48cde9
DEBORAH MOMODU <deborahmomodu999@gmail.com>
2026-01-20 22:50:24 +0100

    lab

--- src/localstorage.jsx
+++ src/localstorage.jsx
@@ -15,6 +15,7 @@ export  function LocalStorageCounter() {
   return (
     <div>
       <h1>{count}</h1>
+      
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
     </div>
